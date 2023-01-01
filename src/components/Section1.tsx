25bb795490af77b8a2830ade2b8e6749aa1afa10
Ahmad Tiar K <ahmad.tiar@loyalto.id>
2023-01-01 14:13:17 +0700

    minor changes

--- src/components/Section1.tsx
+++ src/components/Section1.tsx
@@ -7,7 +7,7 @@ export default function Section1() {
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 col-span-2">
       <img src={BigBanner} alt="logo" className="lg:col-span-2" />
       <h1 className="text-5xl font-extrabold">The Bright Future of Web 3.0?</h1>
-      <div className="flex flex-col justify-between">
+      <div className="flex flex-col justify-start">
         <p className="text-sm leading-relaxed pb-4" style={colorDkGray}>
           We dive into the next evolution of the web that claims to put the
           power of the platforms back into the hands of the people. But is it
